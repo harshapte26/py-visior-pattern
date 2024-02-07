858dd9d0aef80cdac9223f450e0faa8314d3ed7e
Himanshu Kumar <himanshukumar_dr@srmuniv.edu.in>
2024-02-06 19:31:09 -0500

    Adding Variable and Parantheses class

--- visitor_pattern.py
+++ visitor_pattern.py
@@ -33,24 +33,32 @@ class Constant(Expression):
         x = float(s.read().split()[0])
         return Constant(x)
 
-class Op(Expression):
-    def __init__(self, op, l, r):
-        self.op = op
-        self.l = l
-        self.r = r
-        Expression.register_loader("Op", Op.load)
+class Variable(Expression):
+    def __init__(self, name):
+        self.name = name
+        Expression.register_loader("Variable", Variable.load)
 
     def accept(self, visitor):
-        visitor.visit(self)
+        visitor.visit_variable(self)
 
     @staticmethod
     def load(s):
-        op = s.read(1)
-        l = Expression.load(s)
-        r = Expression.load(s)
-        return Op(op, l, r)
+        name = s.read().split()[0]
+        return Variable(name)
 
-# Step 2: Define the Visitor interface
+class Parentheses(Expression):
+    def __init__(self, expr):
+        self.expr = expr
+        Expression.register_loader("Parentheses", Parentheses.load)
+
+    def accept(self, visitor):
+        visitor.visit_parentheses(self)
+
+    @staticmethod
+    def load(s):
+        expr = Expression.load(s)
+        return Parentheses(expr)
+
 class Op(Expression):
     def __init__(self, op, l, r):
         self.op = op
@@ -68,7 +76,6 @@ class Op(Expression):
         r = Expression.load(s)
         return Op(op, l, r)
 
-# Step 2: Define the Visitor interface
 class Visitor(ABC):
     @abstractmethod
     def visit_constant(self, constant:Constant) -> None:
@@ -77,7 +84,14 @@ class Visitor(ABC):
     @abstractmethod
     def visit_op(self, op:Op) -> None:
         pass
+        
+    @abstractmethod
+    def visit_variable(self, variable:Variable) -> None:
+        pass
     
+    @abstractmethod
+    def visit_parentheses(self, parentheses:Parentheses) -> None:
+        pass
 class ComputeVisitor(Visitor):
 
     def __init__(self) -> None:
